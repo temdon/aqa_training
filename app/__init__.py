a5686c0a7bc59a47192bd650e666f8ff87391a2e
Artem <artem.kolesnichenko@objectfirst.com>
2024-10-16 11:39:54 +0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'Artem Kolesnichenko'
+__email__ = 'artem.kolesnichenko@objectfirst.com'
+__date__ = '16.10.2024'
---
+++ app/main/__init__.py
@@ -0,0 +1,22 @@
+__author__ = 'Artem Kolesnichenko'
+__email__ = 'artem.kolesnichenko@objectfirst.com'
+__date__ = '16.10.2024'
+
+import random
+
+
+class Application:
+    def add(self, a: int, b: int) -> int:
+        return a + b
+
+    def get_items_random_order(self) -> tuple:
+        items = ('one', 'two', 'three')
+        result_list = []
+        cnt = 0
+        while cnt < len(items):
+            netx_item = random.choice(items)
+            if netx_item not in result_list:
+                result_list.append(netx_item)
+                cnt += 1
+
+        return tuple(result_list)
